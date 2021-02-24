ed99770cd10a605fca46882cfce8cc0719c1aa44
Brijesh Aghav <cs19btech11047@iith.ac.in>
2021-02-24 17:18:19 +0530

    fixed the hostname in build issie

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,10 @@
-export const hostname = `http://127.0.0.1:8000`;
+/* eslint-disable no-unused-vars */
+var temp = "";
+// eslint-disable-next-line no-undef
+if (process.env.NODE_ENV !== "production") {
+  temp = "http://127.0.0.1:8000";
+} else {
+  temp = "https://ca.elan.org.in";
+}
+export const hostname = `${temp}`;
 export const caportal = `${hostname}/caportal`;
